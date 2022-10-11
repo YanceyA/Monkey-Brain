055770e46b4be28be2fda7625b093a811130ae71
YanceyA <yanceya@gmail.com>
2022-10-11 21:46:46 +1300

    11/10/22 Update

--- data_processor.R
+++ data_processor.R
@@ -227,7 +227,12 @@ new_name_check <-function(write_files)
 weather_update <- function(fxn_results, fxn_weather)
 {
   #DMY date
-  fxn_weather$date <- dmy(fxn_weather$date)
+  #fxn_weather$date <- dmy(fxn_weather$date)
+  
+  
+  #fxn_results <- tt_results
+  #fxn_weather <- tt_weather
+  
   
   source("crypt.R")
   
@@ -343,20 +348,34 @@ new_riders_roster <- new_name_check(write_files = FALSE)
 #Read in results and roster file, process data, and save to RDS files for shiny app.
 ctta_results <- read_xlsx(here("data", "tt_results_master.xlsx")) %>% clean_names() %>% remove_empty(which = c("rows", "cols"))
 
-ctta_roster <- readRDS("data/ctta_roster.RDS") #read_csv(here("data", "ctta_roster.csv"))
+                #ctta_roster <- read_csv(here("data", "ctta_roster.csv")) 
+                #%>% mutate(date_added_to_roster = dmy(date_added_to_roster)) #only required if the dates need to be reformated from text dmy
+                #write_csv(ctta_roster, file = here("data/ctta_roster.csv") )
+                #saveRDS(ctta_roster, file = here("data/ctta_roster.RDS"))
+
+ctta_roster <- readRDS("data/ctta_roster.RDS") 
+
 
 tt_results <- process_raw_tt_data(results = ctta_results , roster = ctta_roster )
 
 #Read in weathers file, process data, and save to RDS files for shiny app.
-tt_weather <- readr::read_csv(here("data","tai_tapu_weather2010-2022.csv")) #%>% dplyr::mutate(date = format(date, "%d-%m-%Y"))
+tt_weather <- readr::read_csv(here("data","tai_tapu_weather2010-2022.csv")) #%>% dplyr::mutate(date = dmy(date))
 
 tt_weather <- weather_update(tt_results, tt_weather) %>% full_join(tt_weather, . , c("date", "tair_c", "rh_percent", "dir_deg_t", "speed_km_hr", "pressure", "density", "wd_cardinal") )
 
-saveRDS(tt_results, file = here("data/tt_results.RDS"))
 write.csv(tt_weather, file = here("data/tai_tapu_weather2010-2022.csv"), row.names = FALSE)
-saveRDS(tt_weather, file = here("data/tai_tapu_weather2010-2022.RDS"))
 
-#write files to shiny app directory
+#write files to TT Analysis Directory
+saveRDS(tt_weather, file = here("data/tai_tapu_weather2010-2022.RDS"))
+saveRDS(tt_results, file = here("data/tt_results.RDS"))
+
+#----HOME-------
+#write files to shiny app directory when home
 saveRDS(tt_results, file = here("C:/Users/yance/OneDrive - Allied Motion Technologies Inc/Desktop/R and BI Projects/CTTA Shiny App/data/tt_results.RDS"))
 saveRDS(tt_weather, file = here("C:/Users/yance/OneDrive - Allied Motion Technologies Inc/Desktop/R and BI Projects/CTTA Shiny App/data/tai_tapu_weather2010-2022.RDS"))
+
+#------WORK--------
+#write files to shiny app directory when at work
+saveRDS(tt_results, file = here("C:/Users/Yancey.Arrington/OneDrive - Allied Motion Technologies Inc/Desktop/R and BI Projects/CTTA Shiny App/data/tt_results.RDS"))
+saveRDS(tt_weather, file = here("C:/Users/Yancey.Arrington/OneDrive - Allied Motion Technologies Inc/Desktop/R and BI Projects/CTTA Shiny App/data/tai_tapu_weather2010-2022.RDS"))
 
